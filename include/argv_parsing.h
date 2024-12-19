809473da51f37c669b1f9e98ae9fa6e7434f4861
qdiaps <qdiaps@gmail.com>
2024-12-19 13:05:24 +0200

    Create base project

---
+++ include/argv_parsing.h
@@ -0,0 +1,6 @@
+#ifndef ARGV_PARSING_H
+#define ARGV_PARSING_H
+
+void argv_init(void);
+
+#endif
---
+++ src/argv_parsing.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "../include/argv_parsing.h"
+
+void argv_init(void) {
+    printf("Argv_parsing init...");
+}
+
+/* int main(int argc, char *argv[]) {
+    argv_init();
+    return 0;
+} */
